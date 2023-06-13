fab9d2249c08fb3e799040b0067ac798dc0d1dd8
Sohibul Weli Mutaali Fielmillah <sohibulwelifb@gmail.com>
2023-06-14 00:42:22 +0800

    memlengkapi class mahasiswa

--- src/universitas/mahasiswa.java
+++ src/universitas/mahasiswa.java
@@ -1,13 +1,41 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package universitas;
 
-/**
- *
- * @author HMEI7
- */
 public class mahasiswa {
+    private String npm;
+    private String nama;
+    private String prodi;
     
+    public mahasiswa(String npm, String nama, String prodi) {
+        this.npm = npm;
+        this.nama = nama;
+        this.prodi = prodi;
+    }
+    public String getNpm() {
+        return npm;
+    }
+
+    public void setNpm(String npm) {
+        this.npm = npm;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getProdi() {
+        return prodi;
+    }
+
+    public void setProdi(String prodi) {
+        this.prodi = prodi;
+    }
+    
+    public String getDetail() {
+        return "NPM: " + npm + "\nNama: " + nama + "\nProdi: " + prodi;
+    }
 }
--- src/universitas/mahasiswaberaksi.java
+++ src/universitas/mahasiswaberaksi.java
@@ -1,13 +1,9 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package universitas;
 
-/**
- *
- * @author HMEI7
- */
 public class mahasiswaberaksi {
-    
+    public static void main(String[] args) {
+        mahasiswa mahasiswa = new mahasiswa("210010010", "Sohibul Weli M.F.", "Teknik Informatika");
+        System.out.println(mahasiswa.getDetail());
+    } 
 }
