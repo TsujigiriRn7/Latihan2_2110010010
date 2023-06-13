b870883262f836b42e2bac9f41458c33e4e6c1ec
Sohibul Weli Mutaali Fielmillah <sohibulwelifb@gmail.com>
2023-06-14 00:34:24 +0800

    membuat class mahasiswa dan mahasiswa beraksi

---
+++ src/universitas/mahasiswa.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package universitas;
+
+/**
+ *
+ * @author HMEI7
+ */
+public class mahasiswa {
+    
+}
