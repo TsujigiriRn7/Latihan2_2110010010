b9fdff743445e7a7526485d828a923dfc70b63a3
Sohibul Weli Mutaali Fielmillah <sohibulwelifb@gmail.com>
2023-06-14 00:50:54 +0800

    membuat class mahasiswa dan mahasiswaberaksi

--- src/universitas/mahasiswaberaksi.java
+++ src/universitas/mahasiswaberaksi.java
@@ -3,7 +3,7 @@ package universitas;
 
 public class mahasiswaberaksi {
     public static void main(String[] args) {
-        mahasiswa mahasiswa = new mahasiswa("210010010", "Sohibul Weli M.F.", "Teknik Informatika");
+        mahasiswa mahasiswa = new mahasiswa("2110010010", "Sohibul Weli M.F.", "Teknik Informatika");
         System.out.println(mahasiswa.getDetail());
     } 
 }
